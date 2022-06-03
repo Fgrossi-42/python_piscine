67034cdbdb28d9b35c18c07e66953b9631cccc89
Flavio Grossi <fgrossi@e3r6p4.42roma.it>
2022-06-03 16:21:41 +0200

    pollllllllllllllllllllllllllllllo

--- week3/Bonus/ex02.py
+++ week3/Bonus/ex02.py
@@ -1,15 +1,18 @@
 import pandas as pd 
 import matplotlib.pyplot as plt
 import numpy as np
+import ast
 
-data = pd.read_csv("titles.csv") 
-result = data["genres"].value_counts()[:10].to_dict()
+df = pd.read_csv("titles.csv") 
+df["genres"] = df["genres"].map(ast.literal_eval)
+result = df.explode("genres")
+popul = result.groupby("genres").mean()["tmdb_popularity"].sort_values(ascending=False)[:10].to_dict()
+print(popul)
 cars = []
 cars2 = []
 cars3 = []
-max = data["genres"].value_counts()[:10].sum()
 i =0
-for key, value in result.items():
+for key, value in popul.items():
     cars.append(key)
     cars2.append(value)
     if i == 0:
@@ -21,29 +24,23 @@ fig, (ax1, ax2) = plt.subplots(1, 2, constrained_layout=True)
 fig.suptitle("NETFLIX &&&&&&&&&& CHILL")
 pathces, texts, autotexts = ax1.pie(cars2, labels=cars, explode=cars3, autopct='%1.2f%%')
 plt.setp(autotexts, size='x-small')
-#plt.title("NETFLIX &&&&&&&&&& CHILL")
-#plt.xlabel('The number of films and shows is: ' + str(max))
 
-result2 = {x: (y, data["genres"].to_dict()[x]) for x, y in data["imdb_score"].sort_values(ascending=False)[:10].to_dict().items()}
+popul2 = result.groupby("genres").mean()["imdb_score"].sort_values(ascending=False)[:10].to_dict()
 
-print(result2)
+print(popul2)
 cars = []
 cars2 = []
 cars3 = []
-# max = data["genres"].
 i =0
-for key, value in result2.items():
-    cars.append(value[1])
-    cars2.append(value[0])
-    print(value[1])
+for key, value in popul2.items():
+    cars.append(key)
+    cars2.append(value)
     if i == 0:
         cars3.append(0.2)
         i = 4
     else:
         cars3.append(0.05)
 pathces, texts, autotexts = ax2.pie(cars2, labels=cars, explode=cars3, autopct='%1.2f%%')
-#plt.title("NETFLIX &&&&&&&&&& CHILL")
-#plt.xlabel('The number of films and shows is: ' + str(max))
 plt.setp(autotexts, size='x-small')
 
 plt.subplots_adjust(left=0.1,
--- week3/Bonus/ex03.py
+++ week3/Bonus/ex03.py
@@ -6,20 +6,17 @@ data = pd.read_csv("titles.csv")
 
 
 data = data[data["production_countries"] == "['US']"]
-result2 = {}
-for x, y in data["tmdb_popularity"].sort_values(ascending=False)[:1].to_dict().items():
-        result2[x] = (y, data["production_countries"].to_dict()[x], data["release_year"].to_dict()[x])
+popul2 = data.groupby("release_year").mean()["tmdb_popularity"].sort_values(ascending=False)[:10].to_dict()
 
-# print(result2)
-
-result3 = data[["production_countries", "release_year" , "tmdb_popularity"]].sort_values(by="tmdb_popularity",ascending=False)[:1]
 cars = []
 cars2 = []
 
 i =0
-cars.append(result3['release_year'].values[0])
-cars2.append(result3['tmdb_popularity'].values[0])
-
+for key, value in popul2.items():
+    cars.append(key)
+    cars2.append(value)
+print(popul2)
+plt.figure(figsize=(10,10))
 plt.pie(cars2, labels=cars, autopct='%1.2f%%')
 plt.subplots_adjust(left=0.1,
                     bottom=0.1, 
