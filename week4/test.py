cf2ac6fad4f69278f41d0ad22f9f8da520b174c6
Flavio Grossi <fgrossi@e3r10p4.42roma.it>
2022-07-04 19:44:15 +0200

    abbiamo aggiunto una cifra, manca l'ultimo grafico

--- week4/test.py
+++ week4/test.py
@@ -13,21 +13,33 @@
 # # result = df["location"].value_counts()
 # # print(result["location"].value_counts())
 
+
+
+from turtle import color
 import pandas as pd 
 import matplotlib.pyplot as plt
 import numpy as np
+import math
+import array as arr
 
 data = pd.read_csv("atlcrime.csv")
-order = data.sort_values(by="npu")
-npu = order["npu"].dropna().unique()
-choice = input("Enter the number of the NPU location you want to see the crimes of:\n\n" + str(npu) + "\n\nenter:")
-result1 = data[data["npu"] == choice]#.value_counts()[3:].to_dict()
-result = result1["crime"].value_counts()
+plot1 = plt.subplot2grid((3, 3), (0, 0), colspan=2, fig= plt.figure(figsize=(20,10)))
+plot2 = plt.subplot2grid((3, 3), (0, 2), rowspan=3, colspan=2)
+plot3 = plt.subplot2grid((3, 3), (1, 0), rowspan=6, colspan=2)
 cars = []
 cars2 = []
 cars3 = []
-max = result.sum()
 i = 0
+
+order = data.sort_values(by="npu")
+npu = order["npu"].dropna().unique()
+choice = input("Enter the number of the NPU location you want to see the crimes of:\n\n" + str(npu) + "\n\nenter: ")
+if choice.upper() not in npu:
+    print("Invalid input")
+    exit()
+result1 = data[data["npu"] == choice.upper()]
+result = result1["crime"].value_counts()
+max = result.sum()
 for key, value in result.items():
     cars.append(key)
     cars2.append(value)
@@ -36,12 +48,37 @@ for key, value in result.items():
         i = 4
     else:
         cars3.append(0)
-plt.figure(figsize=(14,10))
-plt.pie(cars2, labels=None, explode=cars3, startangle=180)
-plt.legend(loc="upper left", labels=cars, bbox_to_anchor=(1,1), ncol=1, fancybox=True, shadow=True, prop={'size': 12})
-plt.title("THE MOST COMMON CRIME IN THE NPU " + choice, fontsize=20, weight="bold", color="black")
-plt.xlabel('the total number of crimes the NPU location ' + choice + ' is ' + str(max), fontsize=12, color='black',
+plot3.pie(cars2, labels=None, explode=cars3, startangle=180)
+plot3.legend(loc="upper left", labels=cars, bbox_to_anchor=(1,1), ncol=1, fancybox=True, shadow=True, prop={'size': 12})
+plot3.set_title("THE MOST COMMON CRIME IN THE NPU " + choice.upper(), fontsize=20, weight="bold", color="black", style="italic")
+plt.xlabel('the total number of crimes the NPU location ' + choice.upper() + ' had is ' + str(max), fontsize=12, color='black',
     fontweight='bold', horizontalalignment='center')
+
+beat = data["beat"].value_counts()
+a = arr.array('i', [0, 0, 0, 0, 0, 0, 0, 0])
+for key, value in beat.items():
+	if str(key)[0] == '1':
+		a[1] += int(value)
+	elif str(key)[0] == '2':
+		a[2] += int(value)
+	elif str(key)[0] == '3':
+		a[3] += int(value)
+	elif str(key)[0] == '4':
+		a[4] += int(value)
+	elif str(key)[0] == '5':
+		a[5] += int(value)
+	elif str(key)[0] == '6':
+		a[6] += int(value)
+	else:
+		a[7] += int(value)
+zones = {'Zone 1':a[1], 'Zone 2':a[2], 'Zone 3':a[3], 'Zone 4':a[4], 'Zone 5':a[5], 'Zone 6':a[6], 'Other Zones':a[7]}
+courses = list(zones.keys())
+values = list(zones.values())
+plot2.bar(courses, values, color ='maroon', width = 0.4)
+# plt.ylabel("No. of crimes beaten for zones")
+plot2.set_title("Division of crimes beaten for Zone", fontsize=18, weight="bold", color="black", style="italic")
+plot2.set_ylabel("No. of crimes beaten for zones", fontsize=12, color='black', weight="bold")
+plt.tight_layout()
 plt.show()
 
 # TROUGH THE YEARS CHECK
--- week4/test2.py
+++ week4/test2.py
@@ -1,7 +1,24 @@
 import pandas as pd 
 import matplotlib.pyplot as plt
 import numpy as np
+import array as arr
 
-data = pd.read_csv("atlcrime.csv")
-choice = data["npu"].unique()
-print(choice)
+df = pd.read_csv("atlcrime.csv")
+df['year'] = pd.DatetimeIndex(df['date']).year
+df['month'] = pd.DatetimeIndex(df['date']).month
+result = df[df["year"] == 2009]
+print ("\n\n2009:\n",result["crime"].value_counts().sum())
+result = df[df["year"] == 2010]
+print ("\n\n2010:\n",result["crime"].value_counts().sum())
+result = df[df["year"] == 2011]
+print ("\n\n2011:\n",result["crime"].value_counts().sum())
+result = df[df["year"] == 2012]
+print ("\n\na2012:\n",result["crime"].value_counts().sum())
+result = df[df["year"] == 2013]
+print ("\n\n2013:\n",result["crime"].value_counts().sum())
+result = df[df["year"] == 2014]
+print ("\n\n2014:\n",result["crime"].value_counts().sum())
+result = df[df["year"] == 2015]
+print ("\n\n2015:\n",result["crime"].value_counts().sum())
+result = df[df["year"] == 2016]
+print ("\n\n2016:\n",result["crime"].value_counts().sum())
