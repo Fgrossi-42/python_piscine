de2c8be197ba173ba9cf70cdd8d1bb9314349f2c
Francesco Drudi <91842334+fdrudi@users.noreply.github.com>
2022-07-08 13:26:15 +0100

    finished

--- week4/test.py
+++ week4/test.py
@@ -1,18 +1,19 @@
-import pandas as pd 
+import pandas as pd
 import matplotlib.pyplot as plt
 import numpy as np
 import array as arr
 
 # variables
 
-plot1 = plt.subplot2grid((3, 3), (0, 0), colspan=2, fig= plt.figure(figsize=(20,10)))
+plot1 = plt.subplot2grid((3, 3), (0, 0), rowspan=2, colspan=2, fig= plt.figure(figsize=(20,12)))
 plot2 = plt.subplot2grid((3, 3), (0, 2), rowspan=3, colspan=2)
-plot3 = plt.subplot2grid((3, 3), (1, 0), rowspan=6, colspan=2)
+plot3 = plt.subplot2grid((3, 3), (2, 0), rowspan=5,  colspan=2)
 cars = []
 cars2 = []
 cars3 = []
 i = 0
 crimes = []
+mean_crimes = []
 years = range(2009, 2017)
 data = pd.read_csv("atlcrime.csv")
 data['year'] = pd.DatetimeIndex(data['date']).year
@@ -28,6 +29,7 @@ if choice.upper() not in npu:
     exit()
 for a in years:
 	result = data[data["year"] == a]
+	mean_crimes.append(result["crime"].value_counts().mean())
 	result1 = result[result["npu"] == choice.upper()].value_counts().sum()
 	crimes.append(result1)
 print(crimes)
@@ -72,10 +74,17 @@ for key, value in result.items():
     else:
         cars3.append(0)
 
-plot1.plot(years, crimes, color='red', linewidth=2, linestyle='dashed', marker='o', markerfacecolor='blue', markersize=12, label='crime', antialiased=True)
-plot1.set_ylim(ymin=0, ymax=5000, auto=True)
-plot1.set_ylabel('the number of crimes', fontsize=12, color='black', weight="bold")
-plot1.set_title('the total number of crimes during the years', fontsize=20, weight="bold", color="black", style="italic")
+leg = []
+leg.append("Crimes NPU " + choice.upper())
+leg.append("Mean NPU")
+plot3.plot(years, crimes, color='red', linewidth=2, linestyle='solid', marker='o', markerfacecolor='blue', markersize=12, label='crime', antialiased=True)
+plot3.plot(years, mean_crimes, color='g', linewidth=1, linestyle='dashed', marker='o', markerfacecolor='y', markersize=9)
+plot3.legend(loc="upper right", labels=leg, shadow=True)
+plot3.set_ylim(ymin=0, ymax=5000, auto=True)
+plot3.set_ylabel('the number of crimes', fontsize=12, color='black', weight="bold")
+plot3.set_title('the total number of crimes during the years', fontsize=20, weight="bold", color="black", style="italic")
+for index in range(len(years)):
+	plot3.text(years[index], crimes[index] + 250, crimes[index], size=12, )
 
 colors = ["r" if int(str(zone)[0]) != i else "b" for i in range(1,8)]
 plot2.bar(courses, values, color=colors, width = 0.4)
@@ -84,11 +93,11 @@ plot2.set_ylabel("No. of crimes beaten for zones", fontsize=12, color='black', w
 plot2.set_xlabel("the NPU you selected is in zone " + str(zone)[0], fontsize=12, color='black', weight="bold")
 
 
-plot3.pie(cars2, labels=None, explode=cars3, startangle=180)
-plot3.legend(loc="upper left", labels=cars, bbox_to_anchor=(1,1), ncol=1, fancybox=True, shadow=True, prop={'size': 12})
-plot3.set_title("THE MOST COMMON CRIME IN THE NPU " + choice.upper(), fontsize=20, weight="bold", color="black", style="italic")
-plot3.set_xlabel('the total number of crimes the NPU location ' + choice.upper() + ' had is ' + str(max), fontsize=12, color='black',
-    fontweight='bold', horizontalalignment='center')
+plot1.pie(cars2, labels=None, explode=cars3, startangle=180)
+plot1.legend(loc="upper left", labels=cars, bbox_to_anchor=(1,1), ncol=1, fancybox=True, shadow=True, prop={'size': 12})
+plot1.set_title("THE MOST COMMON CRIME IN THE NPU " + choice.upper(), fontsize=20, weight="bold", color="black", style="italic")
+plot1.set_xlabel('the total number of crimes the NPU location ' + choice.upper() + ' had is ' + str(max), fontsize=12, color='black',
+	fontweight='bold', horizontalalignment='center')
 
 plt.tight_layout()
 plt.show()
